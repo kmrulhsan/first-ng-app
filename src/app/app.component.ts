e90422289c28cd07ed1361b7b5fae7de4f43cfa4
kmrulhsan <dauntlesskmrul@gmail.com>
2025-02-20 12:41:28 +0600

    tested the rouitng link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
     <app-header/>
     <main>
-    <app-home/>
-    </main>
-
     <router-outlet />
+    </main>
   `,
   styles: [`
     main{
